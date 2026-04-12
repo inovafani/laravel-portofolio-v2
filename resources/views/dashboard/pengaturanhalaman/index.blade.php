e99d4c15ea9998bef201660315473cffa0e91b1e
inovafani <inoveka@gmail.com>
2026-04-13 00:11:03 +0700

    selesai menambahkan menu pengaturan halaman

--- resources/views/dashboard/pengaturanhalaman/index.blade.php
+++ resources/views/dashboard/pengaturanhalaman/index.blade.php
@@ -6,10 +6,32 @@
     <div class="form-group row">
         <label class="col-sm-2">About</label>
         <div class="col-sm-6">
-            <select name="form-control form-control-sm" name="_halaman_about">
-                <option value="">=pilih-</option>
+            <select class="form-control form-control-sm" name="_halaman_about">
+                <option value="">-pilih-</option>
                 @foreach ($datahalaman as $item)
-                    <option value="{{$item->id}}">{{$item->judul}}</option>
+                    <option value="{{$item->id}}" {{get_meta_value('_halaman_about')==$item->id?'selected' : ''}}>{{$item->judul}}</option>
+                @endforeach
+            </select>
+        </div>
+    </div>
+    <div class="form-group row">
+        <label class="col-sm-2">Interest</label>
+        <div class="col-sm-6">
+            <select class="form-control form-control-sm" name="_halaman_interest">
+                <option value="">-pilih-</option>
+                @foreach ($datahalaman as $item)
+                    <option value="{{$item->id}}" {{get_meta_value('_halaman_interest')==$item->id?'selected' : ''}}>{{$item->judul}}</option>
+                @endforeach
+            </select>
+        </div>
+    </div>
+    <div class="form-group row">
+        <label class="col-sm-2">Award</label>
+        <div class="col-sm-6">
+            <select class="form-control form-control-sm" name="_halaman_award">
+                <option value="">-pilih-</option>
+                @foreach ($datahalaman as $item)
+                    <option value="{{$item->id}}" {{get_meta_value('_halaman_award')==$item->id?'selected' : ''}}>{{$item->judul}}</option>
                 @endforeach
             </select>
         </div>
