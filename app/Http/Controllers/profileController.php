bfa977f2f68314ba74f3ff03e4c3d398eb8d40fb
inovafani <inoveka@gmail.com>
2026-04-12 13:49:34 +0700

    menambahkan crud untuk isian portofolio

--- app/Http/Controllers/profileController.php
+++ app/Http/Controllers/profileController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\metadata;
 use Illuminate\Http\Request;
 
 class profileController extends Controller
@@ -10,7 +11,27 @@ class profileController extends Controller
         return view('dashboard.profile.index');
     }
 
-    function update(){
+    function update(Request $request){
+        $request->validate([
+            '_foto' => 'mimes:jpeg,jpg,png,gif',
+            '_email' => 'required|email',
+        ],[
+            '_foto.mimes' => 'Foto yang dimasukkan hanya diperbolehkan berekstensi JPEG, JPG, PNG, dan GIF',
+            '_email.required' => 'Email wajib diisi',
+            '_email.email' => 'Format email yang dimasukkan tidak valid',
+        ]);
 
+        if($request->hasFile('_foto')) {
+            $foto_file = $request->file('_foto');
+            $foto_ekstensi = $foto_file->extension();
+            $foto_baru = date('ymdhis').".$foto_ekstensi";
+            $foto_file->move(public_path('foto'), $foto_baru);
+
+            metadata::updateOrCreate(['meta_key' => '_foto'], ['meta_value' => $foto_baru]);
+        }
+
+        metadata::updateOrCreate(['meta_key' => '_email'], ['meta_value' => $request->_email]);
+
+        return redirect()->route('profile.index')->with('success', 'Berhasil update data profile');
     }
 }
--- resources/views/dashboard/profile/index.blade.php
+++ resources/views/dashboard/profile/index.blade.php
@@ -1,38 +1,56 @@
 @extends('dashboard.layout')
 
 @section('konten')
-<form action="{{ route('skill.update') }}" method="POST">
+<form action="{{ route('profile.update') }}" method="POST" enctype="multipart/form-data">
     @csrf
-    <div class="mb-3">
-        <label for="judul" class="form-label">Programming Language & Tools</label>
-        <input
-            type="text"
-            class="form-control form-control-sm skill"
-            name="_language"
-            id="judul"
-            aria-describedby="helpId"
-            placeholder="Masukkan programming language dan tools"
-            value="{{ get_meta_value('_language') }}"
-        />
+    <div class="row justify-content-between">
+        <div class="col-5">
+            <h3>Profile</h3>
+            @if (get_meta_value('_foto'))
+                <img style="max-width:100px;max-height:100px" src="{{ asset('foto')."/".get_meta_value('_foto') }}" alt="">
+            @endif
+            <div class="mb-3">
+                <label for="_foto" class="form-label">Foto</label>
+                <input type="file" class="form-control form-control-sm" name="_foto" id="_foto">
+            </div>
+            <div class="mb-3">
+                <label for="_kota" class="form-label">Kota</label>
+                <input type="text" class="form-control form-control-sm" name="_kota" id="_kota">
+            </div>
+            <div class="mb-3">
+                <label for="_provinsi" class="form-label">Provinsi</label>
+                <input type="text" class="form-control form-control-sm" name="_provinsi" id="_provinsi">
+            </div>
+            <div class="mb-3">
+                <label for="_nohp" class="form-label">Nomor HP</label>
+                <input type="text" class="form-control form-control-sm" name="_nohp" id="_nohp">
+            </div>
+            <div class="mb-3">
+                <label for="_email" class="form-label">Email</label>
+                <input type="text" class="form-control form-control-sm" name="_email" id="_email" value="{{ get_meta_value('_email') }}">
+            </div>
+        </div>
+        <div class="col-5">
+            <h3>Akun Sosial Media</h3>
+            <div class="mb-3">
+                <label for="_facebook" class="form-label">Facebook</label>
+                <input type="text" class="form-control form-control-sm" name="_facebook" id="_facebook">
+            </div>
+            <div class="mb-3">
+                <label for="_twitter" class="form-label">Twitter</label>
+                <input type="text" class="form-control form-control-sm" name="_twitter" id="_twitter">
+            </div>
+            <div class="mb-3">
+                <label for="_linkedin" class="form-label">LinkedIn</label>
+                <input type="text" class="form-control form-control-sm" name="_linkedin" id="_linkedin">
+            </div>
+            <div class="mb-3">
+                <label for="_github" class="form-label">Github</label>
+                <input type="text" class="form-control form-control-sm" name="_github" id="_github">
+            </div>
+        </div>
     </div>
-    <div class="mb-3">
-        <label for="judul" class="form-label">Workflow</label>
-        <textarea name="_workflow" id="summernote" class="form-control" rows="5">{{ get_meta_value('_workflow') }}</textarea>
-    </div>
+    
     <button class="btn btn-primary" name="simpan" type="submit">Submit</button>
 </form>
 @endsection
-
-@push('child-scripts')
-    <script>
-    $(document).ready(function() {
-        $('.skill').tokenfield({
-            autocomplete: {
-                source: [{!! $skill !!}],
-                delay: 100
-            },
-            showAutocompleteOnFocus: true
-        });
-    });
-</script>
-@endpush
